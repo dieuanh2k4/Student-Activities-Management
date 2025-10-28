5915f97791d18c6f5fb1bd7350cdd1b7dcae14af
Văn Đức Anh <ducanhku@gmail.com>
2025-10-29 05:54:57 +0700

    say hi againagain

--- obj/Debug/net8.0/StudentActivities.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentActivities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentActivities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a07926819584686f879f5fb870fcca9d5990a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210fe91ddd8ed9c65247af90100452af35a09a14")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentActivities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentActivities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
