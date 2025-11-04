cfde2b0b7484d2e97f6b4f11c9e3f4c7da843753
Hoaix <zeusedom@gmail.com>
2025-11-04 23:57:17 +0700

    feat: tao CreateClubDto voi OrganizerId

---
+++ src/Dtos/Clubs/CreateClubDto.cs
@@ -0,0 +1,11 @@
+namespace StudentActivities.src.Dtos.Clubs
+{
+    public class CreateClubDto
+    {
+        public string Name { get; set; } = null!;
+        public string? Thumbnail { get; set; }
+        public string? Description { get; set; }
+        public int MaxCapacity { get; set; }
+        public int OrganizerId { get; set; }
+    }
+}
