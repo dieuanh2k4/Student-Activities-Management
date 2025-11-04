6402da253b67815054508542373097b598e63504
Hoaix <zeusedom@gmail.com>
2025-11-04 23:55:36 +0700

    feat: thêm OrganizerId và liên kết Organizers vào Clubs

--- src/Models/Clubs.cs
+++ src/Models/Clubs.cs
@@ -2,20 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StudentActivities.src.Models;
 
 namespace StudentActivities.src.Models
 {
     public class Clubs
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public string Name { get; set; } = null!;
         public string? Thumbnail { get; set; }
         public string? Description { get; set; }
-        public int MaxCapacity { get; set; } // số lượng người tham gia tối đa
-        public int OrganizerId { get; set; }
-        
+        public int MaxCapacity { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public int OrganizerId { get; set; } // Thêm FK
         public Organizers? Organizers { get; set; }
+
         public List<Notifications>? Notifications { get; set; }
-        public Resgistrations? Resgistrations { get; set; }
+        public List<Registrations>? Registrations { get; set; }
     }
 }
