0bf247e7cff82afb96c55062c1520b154a5ef6ed
Hoaix <zeusedom@gmail.com>
2025-11-05 02:15:35 +0700

    feat: them UpdateClubDto cho cap nhat club (truong nullable)

---
+++ src/Dtos/Clubs/UpdateClubDto.cs
@@ -0,0 +1,11 @@
+namespace StudentActivities.src.Dtos.Clubs
+{
+    public class UpdateClubDto
+    {
+        public string? Name { get; set; }
+        public string? Thumbnail { get; set; }
+        public string? Description { get; set; }
+        public int? MaxCapacity { get; set; }
+        public int? OrganizerId { get; set; }
+    }
+}
