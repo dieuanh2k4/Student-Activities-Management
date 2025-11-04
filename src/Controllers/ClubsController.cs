0e344199b71750f86b300689a7035ca8f183159b
Hoaix <zeusedom@gmail.com>
2025-11-05 02:14:51 +0700

    feat: them ClubsController voi cac API CRUD cho Club

---
+++ src/Controllers/ClubsController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using StudentActivities.src.Dtos.Clubs;
+using StudentActivities.src.Services.Interfaces;
+
+namespace StudentActivities.src.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClubsController : ControllerBase
+    {
+        private readonly IClubService _svc;
+
+        public ClubsController(IClubService svc) => _svc = svc;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _svc.GetAllAsync());
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var item = await _svc.GetByIdAsync(id);
+            return item == null ? NotFound() : Ok(item);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateClubDto dto)
+        {
+            var created = await _svc.CreateAsync(dto);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateClubDto dto)
+            => await _svc.UpdateAsync(id, dto) ? NoContent() : NotFound();
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+            => await _svc.DeleteAsync(id) ? NoContent() : NotFound();
+    }
+}
