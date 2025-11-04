93d5c6173af299a2775453f2d498cd87d93ddbae
Hoaix <zeusedom@gmail.com>
2025-11-04 23:56:51 +0700

    feat: tao UpdateClubDto cho chinh sua câu lac bo

--- src/Dtos/Clubs/UpdateClubDto.cs
+++ src/Dtos/Clubs/UpdateClubDto.cs
@@ -6,5 +6,6 @@ namespace StudentActivities.src.Dtos.Clubs
         public string? Thumbnail { get; set; }
         public string? Description { get; set; }
         public int? MaxCapacity { get; set; }
+        public int? OrganizerId { get; set; }
     }
 }
