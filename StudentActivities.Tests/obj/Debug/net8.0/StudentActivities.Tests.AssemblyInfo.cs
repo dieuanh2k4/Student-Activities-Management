cc6dad6a5e2a85e0c2e9372a2e7a44ade67a292a
Nguyễn Diệu Anh <121100269+dieuanh2k4@users.noreply.github.com>
2026-05-05 21:38:10 +0700

    UML

--- StudentActivities.Tests/obj/Debug/net8.0/StudentActivities.Tests.AssemblyInfo.cs
+++ StudentActivities.Tests/obj/Debug/net8.0/StudentActivities.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentActivities.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdac1898e3070fdde3712f76ba6bee7a2ce8486f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d03ae40d7d7381040b42ee0889e04d4139e4215")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentActivities.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentActivities.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
